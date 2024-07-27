817bcd668bb7329992db210d6c4b224059862fcc
Jaswant Singh <jaswant9461412627@gmail.com>
2024-07-27 13:15:32 +0530

    Adding Google Anlytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import Navbar from "../components/Nav";
+import Script from "next/script";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 export const metadata = {
   title: "Jaswant Singh's Porfolio",
@@ -10,8 +11,25 @@ export const metadata = {
 const Rootlayout = ({ children }) => {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <Script id="ga-script" strategy="lazyOnload">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+      page_path: window.location.pathname,
+    });
+        `}
+        </Script>
+      </head>
       <body >
-      <div className="main"></div>
+        <div className="main"></div>
         <main className="app">
           <Navbar />
           {children}
